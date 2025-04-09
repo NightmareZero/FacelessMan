7a4451532df5ec1ffd757a4cd3a8e9d3bfbfd89b
Nightmare <nightzhuxy@hotmail.com>
2025-04-09 18:01:11 +0800

    调整一些逻辑

--- Source/Abilities/Common/EvAbility.cs
+++ Source/Abilities/Common/EvAbility.cs
@@ -12,16 +12,39 @@ namespace NzFaceLessManMod
     {
         public override void BeforeStartCooldown(int ticks)
         {
-            float cdVal = pawn.GetStatValue(XmlDefs.NzFlm_SkillCooldownTime, true);
+            float cdVal = pawn.GetStatValue(XmlDefs.NzFlm_EvSkillCooldownTime, true);
             float fTicks = ticks * cdVal;
             base.BeforeStartCooldown((int)fTicks);
         }
 
         public override void BeforeNotify_GroupStartedCooldown(AbilityGroupDef group, int ticks)
         {
-            float cdVal = pawn.GetStatValue(XmlDefs.NzFlm_SkillCooldownTime, true);
+            float cdVal = pawn.GetStatValue(XmlDefs.NzFlm_EvSkillCooldownTime, true);
             float fTicks = ticks * cdVal;
             base.BeforeNotify_GroupStartedCooldown(group, (int)fTicks);
+        }
+
+
+
+
+        public EvAbility() : base()
+        {
+        }
+
+        public EvAbility(Pawn pawn) : base(pawn)
+        {
+        }
+
+        public EvAbility(Pawn pawn, Precept sourcePrecept) : base(pawn, sourcePrecept)
+        {
+        }
+
+        public EvAbility(Pawn pawn, AbilityDef def) : base(pawn, def)
+        {
+        }
+
+        public EvAbility(Pawn pawn, Precept sourcePrecept, AbilityDef def) : base(pawn, sourcePrecept, def)
+        {
         }
     }
 }
