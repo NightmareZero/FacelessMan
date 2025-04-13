87b6630343cbbb61db36a6197262d380ee04d17b
Nightmare <nightzhuxy@hotmail.com>
2025-04-13 16:41:57 +0800

    修复一些技能

--- Source/Things/Comp_GlancingHit.cs
+++ Source/Things/Comp_GlancingHit.cs
@@ -27,12 +27,18 @@ namespace NzFaceLessManMod
             // 没有加害者
             if (dinfo.Instigator == null)
             {
+// #if DEBUG
+//                 Log.Warning("ThingComp_GlancingHit_没有加害者");
+// #endif
                 return;
             }
 
             // 加害者是自身
             if (dinfo.Instigator == parent)
             {
+// #if DEBUG
+//                 Log.Warning("ThingComp_GlancingHit_加害者是自身");
+// #endif                
                 return;
             }
 
@@ -40,10 +46,15 @@ namespace NzFaceLessManMod
             float gh = parent.GetStatValue(XmlDefs.NzFlm_GlancingHitPoint, cacheStaleAfterTicks: 600);
             if (dinfo.Amount < gh)
             {
+// #if DEBUG
+//                 Log.Warning(string.Format("ThingComp_GlancingHit_偏斜值 {0} 大于伤害值 {1}", gh, dinfo.Amount));
+// #endif
                 absorbed = true; // 吸收伤害
                 return;
             }
-
+// #if DEBUG
+//             Log.Warning(string.Format("ThingComp_GlancingHit_偏斜值 {0} 最初伤害值 {1} ", gh, dinfo.Amount));
+// #endif
             // 计算偏斜值
             dinfo.SetAmount(dinfo.Amount - gh); // 减少伤害
         }
