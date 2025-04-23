09726922af7fe4f732ca86ce8c29481361f10bf0
Nightmare <nightzhuxy@hotmail.com>
2025-04-23 18:29:47 +0800

    调整一些描述逻辑

--- Source/Hediffs/Hediff_MindWormMaster.cs
+++ Source/Hediffs/Hediff_MindWormMaster.cs
@@ -9,9 +9,11 @@ namespace NzFaceLessManMod
 {
     public class Hediff_MindWormMaster : HediffWithComps
     {
-        public HashSet<Hediff> mindWorms = new HashSet<Hediff>(); // 心灵蠕虫列表
+        private HashSet<Hediff> mindWorms = new HashSet<Hediff>(); // 心灵蠕虫列表
 
-        public HashSet<Pawn> slaves = new HashSet<Pawn>(); // 奴隶列表
+        private HashSet<Pawn> slaves = new HashSet<Pawn>(); // 奴隶列表
+
+        private bool dirtyCaches = true; // 是否需要更新缓存
 
         public bool addWorm(Hediff worm)
         {
@@ -46,6 +48,7 @@ namespace NzFaceLessManMod
                 Log.Error("MindWormMaster: delWorm failed, worm not found in mindWorms list.");
                 return false;
             }
+
             mindWorms.Remove(worm);
             if (worm.pawn != null)
             {
@@ -56,6 +59,8 @@ namespace NzFaceLessManMod
                 updateSlavesFromMindWorms(); // 更新奴隶列表
             }
 
+            dirtyCaches = true; // 标记缓存需要更新
+            updateCaches(); // 更新缓存
 
             return true;
         }
@@ -90,6 +95,61 @@ namespace NzFaceLessManMod
             if (Scribe.mode == LoadSaveMode.PostLoadInit)
             {
                 updateSlavesFromMindWorms();
+                updateCaches();
+            }
+        }
+
+        public void updateCaches(bool force = false)
+        {
+            if (!dirtyCaches && !force)
+            {
+                return;
+            }
+            try
+            {
+                // 更新描述
+                if (slaves != null && slaves.Count > 0)
+                {
+                    for (int i = 0; i < slaves.Count; i++)
+                    {
+                        cacheDescription += slaves.ElementAt(i).Name.ToStringShort + "\n ";
+                    }
+                }
+
+            }
+            
+            finally
+            {
+                dirtyCaches = false;
+            }
+        }
+        private string cacheDescription = null; // 描述
+
+        public override string LabelBase
+        {
+            get
+            {
+                string baseLabel = base.LabelBase;
+                if (slaves.Count > 0)
+                {
+                    return baseLabel + "nzflm.slaves_count".Translate(slaves.Count.ToString());
+                }
+                return baseLabel;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                string baseDescription = base.Description;
+                if (slaves.Count > 0)
+                {
+                    updateCaches(); // 更新缓存
+                    return baseDescription + "\n" + "nzflm.slaves_names".Translate(cacheDescription);
+                }
+                return baseDescription;
+
             }
         }
     }
