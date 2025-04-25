cf79221064c6852cb8ca5b4ddd773aaf064b90f3
Nightmare <nightzhuxy@hotmail.com>
2025-04-25 16:34:21 +0800

    添加一些默认值

--- Source/Genes/EvolutionGeneDef.cs
+++ Source/Genes/EvolutionGeneDef.cs
@@ -4,20 +4,21 @@ using RimWorld;
 using Verse;
 
 namespace NzFaceLessManMod
-{ 
-    
+{
+
     public class EvolutionGeneDef : GeneDef, IComparable<EvolutionGeneDef>
     {
         // 取得该进化要求的点数
         public int evolution = 0;
 
         // 不可自选
-        public bool cannotBeChosen;
+        public bool cannotBeChosen = false;
 
         // 当移除时警告
         public bool warnOnRemove = false;
         public string warnOnRemoveText = null;
 
+        // 是否可以在基因组中生成
         public new bool canGenerateInGeneSet = false;
 
         // 前置要求
@@ -33,7 +34,7 @@ namespace NzFaceLessManMod
         public List<string> ExclusionTags => exclusionTags;
 
         public EvolutionGeneDef()
-        { 
+        {
             geneClass = typeof(GeneExt);
         }
 
@@ -41,5 +42,7 @@ namespace NzFaceLessManMod
         {
             return this.displayOrderInCategory.CompareTo(other.displayOrderInCategory);
         }
+        
+        
     }
 }
